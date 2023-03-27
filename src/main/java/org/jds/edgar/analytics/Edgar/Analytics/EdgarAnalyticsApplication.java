a6d691f6d29eca288d9a20e77ccfd91c3bdc347d
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-03-27 14:54:02 +0400

    Initial

---
+++ src/main/java/org/jds/edgar/analytics/Edgar/Analytics/EdgarAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package org.jds.edgar.analytics.Edgar.Analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgarAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgarAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jds/edgar/analytics/Edgar/Analytics/EdgarAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jds.edgar.analytics.Edgar.Analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgarAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
